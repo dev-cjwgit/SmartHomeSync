098ae2b76014e855de3cb7d3563524608108a8ab
최진우 <cjw.git@gmail.com>
2023-09-24 16:44:27 +0900

    feat : 예외 처리 구조 생성

---
+++ src/main/java/com/cjw/smarthomesync/common/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cjw.smarthomesync.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<?> handler(Throwable t) {
+        log.error(t.getMessage());
+
+        return new ResponseEntity<Object>(new HashMap<String, Object>() {{
+            put("result", false);
+            put("msg", "알 수 없는 예외입니다.");
+        }}, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+}
---
+++ src/main/java/com/cjw/smarthomesync/common/exception/ErrorMessage.java
@@ -0,0 +1,23 @@
+package com.cjw.smarthomesync.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorMessage {
+    ACCESS_TOKEN_EXPIRE(1000, "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_INVALID(1001, "토큰이 잘못되었습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_NOT_LOAD(1002, "토큰을 불러오지 못하였습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_INVALID_STRUCT(1010, "토큰이 구조가 잘못되었습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_INVALID_HEADER(1011, "토큰 해더가 손상되었습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_INVALID_PAYLOADS(1012, "토큰 정보가 손상되었습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_INVALID_SIGNATURE(1013, "토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_EMPTY(1014, "토큰이 입력되지 않았습니다.", HttpStatus.BAD_REQUEST),
+    UNDEFINED_EXCEPTION(0, "정의 되지 않은 오류 입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private final Integer code;
+    private final String message;
+    private final HttpStatus httpStatus;
+}
