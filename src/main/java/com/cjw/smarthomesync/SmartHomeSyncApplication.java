e084ba26df7c48b49e3ac919fb335187cd74fb55
최진우 <cjw.git@gmail.com>
2023-09-24 16:47:38 +0900

    init : 프로젝트 구조 생성

---
+++ src/main/java/com/cjw/smarthomesync/SmartHomeSyncApplication.java
@@ -0,0 +1,13 @@
+package com.cjw.smarthomesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cjw/smarthomesync/SmartHomeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjw.smarthomesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
