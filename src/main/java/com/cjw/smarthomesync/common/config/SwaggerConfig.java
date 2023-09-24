88b1b9ec9516ace9a486deb131e6f6b3bcf18c69
최진우 <cjw.git@gmail.com>
2023-09-24 17:11:38 +0900

    feat : Swagger Authorize 기능 추가

--- src/main/java/com/cjw/smarthomesync/common/config/SwaggerConfig.java
+++ src/main/java/com/cjw/smarthomesync/common/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ package com.cjw.smarthomesync.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,8 +17,23 @@ public class SwaggerConfig {
                 .title("SmartHomeSync API Docs")
                 .version("v0.1")
                 .description("SmartHomeSync API 명세서");
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("Bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
