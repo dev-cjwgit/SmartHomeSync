15ae3a5cb7fc06d4a661a911177c171447799d63
최진우 <cjw.git@gmail.com>
2023-09-30 11:49:04 +0900

    fix : 사용자 예외 핸들링
    현재 BaseException 구조가 좋은 구조가 아닌 것 같다.
    ErrorLog를 분석하고 이에 맞게 수정이 필요하다.

--- src/main/java/com/cjw/smarthomesync/common/config/GlobalExceptionHandler.java
+++ src/main/java/com/cjw/smarthomesync/common/config/GlobalExceptionHandler.java
@@ -1,25 +1,44 @@
 package com.cjw.smarthomesync.common.config;
 
+import com.cjw.smarthomesync.common.domain.ResponseDto;
+import com.cjw.smarthomesync.common.exception.BaseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ResponseDto<?>> handler(BaseException t) {
+        List<String> errorList = t.getErrorMessage();
+        return new ResponseEntity<>(
+                ResponseDto.<String>builder()
+                        .result(false)
+                        .message("요청에 응답을 실패하였습니다.")
+                        .size(errorList.size())
+                        .data(errorList)
+                        .build()
+                , HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(Throwable.class)
-    public ResponseEntity<?> handler(Throwable t) {
+    public ResponseEntity<ResponseDto<?>> handler(Throwable t) {
         log.error(t.getCause().toString());
 
-        return new ResponseEntity<Object>(new HashMap<String, Object>() {{
-            put("result", false);
-            put("msg", "알 수 없는 예외입니다.");
-        }}, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(
+                ResponseDto.builder()
+                        .result(false)
+                        .message("알 수 없는 예외 입니다.")
+                        .size(0)
+                        .data(Collections.emptyList())
+                        .build()
+                , HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 }
