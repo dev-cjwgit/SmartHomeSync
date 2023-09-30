2f2cc7d7960534286282fd46bb5bb7d69c5029aa
최진우 <cjw.git@gmail.com>
2023-09-30 16:12:43 +0900

    fix : 닉네임 및 이름 정규식 대문자 허용

--- src/main/java/com/cjw/smarthomesync/auth/domain/request/SignupVo.java
+++ src/main/java/com/cjw/smarthomesync/auth/domain/request/SignupVo.java
@@ -20,10 +20,10 @@ public class SignupVo {
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=\\S+$).{8,30}", groups = {ValidationGroups.signup.class}, message = "비밀 번호는 8~30자 특수 문자 대,소문자 그리고 숫자가 포함 되어야 합니다.")
     private String password;
 
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,20}$", groups = {ValidationGroups.signup.class}, message = "닉네임은 특수 문자를 제외한 2~20자리 여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,20}$", groups = {ValidationGroups.signup.class}, message = "이름은 특수 문자를 제외한 2~20자리 여야 합니다.")
     private String name;
 
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", groups = {ValidationGroups.signup.class}, message = "닉네임은 특수 문자를 제외한 2~10자리 여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", groups = {ValidationGroups.signup.class}, message = "닉네임은 특수 문자를 제외한 2~10자리 여야 합니다.")
     private String nickname;
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", groups = {ValidationGroups.signup.class}, message = "핸드폰 번호의 양식과 맞지 않습니다. 01X-XXX(X)-XXXX")
