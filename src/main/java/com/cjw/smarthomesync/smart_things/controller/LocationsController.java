7e6ce5b7d79920009115c71d403cced77026eea6
최진우 <cjw.git@gmail.com>
2023-10-04 15:27:39 +0900

    refactor : Controller Authentcation 가져오기

--- src/main/java/com/cjw/smarthomesync/smart_things/controller/LocationsController.java
+++ src/main/java/com/cjw/smarthomesync/smart_things/controller/LocationsController.java
@@ -1,9 +1,13 @@
 package com.cjw.smarthomesync.smart_things.controller;
 
+import com.cjw.smarthomesync.common.domain.AuthEntity;
 import com.cjw.smarthomesync.common.domain.ResponseDto;
+import com.cjw.smarthomesync.common.exception.BaseException;
+import com.cjw.smarthomesync.common.exception.ErrorMessage;
 import com.cjw.smarthomesync.smart_things.service.LocationsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +22,13 @@ public class LocationsController {
     private final LocationsService locationsService;
 
     @GetMapping()
-    public ResponseEntity<ResponseDto<Map<?, ?>>> getLocationsData() {
+    public ResponseEntity<ResponseDto<Map<?, ?>>> getLocationsData(final Authentication authentication) {
+        Object temp = authentication.getPrincipal();
+        if (!(temp instanceof AuthEntity))
+            throw new BaseException(ErrorMessage.UNDEFINED_EXCEPTION);
+
+        AuthEntity auth = (AuthEntity) temp;
+
         Map<?, ?> result = locationsService.getLocationsData();
 
         return ResponseEntity.ok(ResponseDto.<Map<?, ?>>builder()
--- src/main/java/com/cjw/smarthomesync/smart_things/controller/RoomsController.java
+++ src/main/java/com/cjw/smarthomesync/smart_things/controller/RoomsController.java
@@ -1,9 +1,13 @@
 package com.cjw.smarthomesync.smart_things.controller;
 
+import com.cjw.smarthomesync.common.domain.AuthEntity;
 import com.cjw.smarthomesync.common.domain.ResponseDto;
+import com.cjw.smarthomesync.common.exception.BaseException;
+import com.cjw.smarthomesync.common.exception.ErrorMessage;
 import com.cjw.smarthomesync.smart_things.service.RoomsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +23,13 @@ public class RoomsController {
     private final RoomsService roomsService;
 
     @GetMapping("/{location_id}")
-    public ResponseEntity<ResponseDto<Map<?, ?>>> getRoomsData(@PathVariable("location_id") String locationId) {
+    public ResponseEntity<ResponseDto<Map<?, ?>>> getRoomsData(final Authentication authentication, @PathVariable("location_id") String locationId) {
+        Object temp = authentication.getPrincipal();
+        if (!(temp instanceof AuthEntity))
+            throw new BaseException(ErrorMessage.UNDEFINED_EXCEPTION);
+
+        AuthEntity auth = (AuthEntity) temp;
+
         Map<?, ?> result = roomsService.getRoomsData(locationId);
 
         return ResponseEntity.ok(ResponseDto.<Map<?, ?>>builder()
